c3061a7f7bc043db1d0fc974557460b13f3ab4a8
Corentin-A <avril.corentin@gmail.com>
2024-08-01 13:44:29 -0400

    change stat data from data to statistics delete data.ts

--- app/clinic-client/portal/Constants/Constants.ts
+++ app/clinic-client/portal/Constants/Constants.ts
@@ -21,3 +21,40 @@ export const navLinks = [
     },
   ];
   
+  export const stats = [
+    {
+      id: 1,
+      image: "/images/important.png",
+      name: "Maj récente",
+      maj: 0, 
+      nb: 5,
+    },
+    {
+      id: 2,
+      image: "/images/commandes.png",
+      name: "Commandes en cours",
+      maj: 5,
+      nb: 10,
+    },
+    {
+      id: 3,
+      image: "/images/people.png",
+      name: "Nombre de patients",
+      maj: 2,
+      nb: 50,
+    },
+    {
+      id: 4,
+      image: "/images/commandes_terminees.png",
+      name: "Commandes terminées",
+      maj: 8,
+      nb: 60,
+    },
+    {
+      id: 5,
+      image: "/images/commandes_archivees.png",
+      name: "Commandes archivées",
+      maj: 10,
+      nb: 212,
+    },
+  ];
