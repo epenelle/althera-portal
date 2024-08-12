dad95330f09e70741b7e120657e04f3333b62020
epenelle <etienne.penelle@gmail.com>
2024-08-11 20:52:44 -0400

    rm space

--- app/clinic-client/portal/components/Home/Statistics/Statistics.tsx
+++ app/clinic-client/portal/components/Home/Statistics/Statistics.tsx
@@ -6,7 +6,7 @@ import { IoBarChartOutline } from 'react-icons/io5';
 const Statistics = () => {
   return (
     <div className='ml-[10vh] md:ml-[15vh]'>
-        <div className='w-4/5 mx-auto p-6 bg-light-white '>
+        <div className='w-4/5 mx-auto p-6 bg-light-white'>
             <div className='border-light-gray border-b-2 flex items-center justify-center pb-4'>
                 <IoBarChartOutline size={30} className="mr-2" />
                 <h1 className='text-secondary-dark-blue text-center font-bold md:text-4x1 p-2 text-3xl'>Tableau de bord</h1>
