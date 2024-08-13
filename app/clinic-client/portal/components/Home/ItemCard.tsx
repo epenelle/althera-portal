05f38d79a651a25986bb7d55cdb7a8bbca1dfe19
epenelle <etienne.penelle@gmail.com>
2024-08-12 21:49:38 -0400

    change patient icon

--- app/clinic-client/portal/components/Home/ItemCard.tsx
+++ app/clinic-client/portal/components/Home/ItemCard.tsx
@@ -4,8 +4,8 @@ import { useRouter } from 'next/router';
 import React from 'react'
 import { IoIosArrowForward } from 'react-icons/io';
 import { format } from 'date-fns';
+import { BsPersonFill } from 'react-icons/bs';
 import { RiListUnordered } from 'react-icons/ri';
-import { FaUserDoctor } from 'react-icons/fa6';
 
 type Props = {
     data: any;
@@ -26,7 +26,7 @@ const PatientCard = ({data}: Props) => {
         <div className='border-light-gray border-2 border-opacity-30 cursor-pointer hover:shadow-md mb-2 p-5 rounded-2xl sm:p-3 transition-all duration-150'>
                 {isPatient(data) ? (
                     <div onClick={() => handleClick('patient', data.id.toString())} className='grid-cols-1 grid items-center gap-4 sm:grid-cols-2 sm:gap-2 md:grid-cols-[100px_150px_150px_1fr_24px]'>
-                        <FaUserDoctor size={48} className='font-bold text-secondary-dark-blue md:block sm:hidden' />
+                        <BsPersonFill size={48} className='font-bold text-secondary-dark-blue md:block sm:hidden' />
                         <h1 className='font-bold text-base text-secondary-dark-blue truncate'>{data.lastName}</h1>
                         <h1 className='font-bold text-base text-secondary-dark-blue truncate'>{data.firstName}</h1>
                         <h1 className='font-bold text-base text-secondary-dark-blue truncate'>{data.healthInsuranceNumber}</h1>
