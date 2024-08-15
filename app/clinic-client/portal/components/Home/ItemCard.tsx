0fee809b4c9953892a05b10d2683f4419b65f7ca
Corentin-A <avril.corentin@gmail.com>
2024-08-15 18:09:15 -0400

    fix

--- app/clinic-client/portal/components/Home/ItemCard.tsx
+++ app/clinic-client/portal/components/Home/ItemCard.tsx
@@ -27,8 +27,8 @@ const PatientCard = ({data}: Props) => {
                 {isPatient(data) ? (
                     <div onClick={() => handleClick('patient', data.id.toString())} className='grid-cols-1 grid items-center gap-4 sm:grid-cols-2 sm:gap-2 md:grid-cols-[100px_150px_150px_1fr_24px]'>
                         <BsPersonFill size={48} className='font-bold text-secondary-dark-blue md:block sm:hidden' />
-                        <h1 className='font-bold text-base text-secondary-dark-blue truncate'>{data.lastName}</h1>
                         <h1 className='font-bold text-base text-secondary-dark-blue truncate'>{data.firstName}</h1>
+                        <h1 className='font-bold text-base text-secondary-dark-blue truncate'>{data.lastName}</h1>
                         <h1 className='font-bold text-base text-secondary-dark-blue truncate'>{data.healthInsuranceNumber}</h1>
                         <IoIosArrowForward size={24} className='font-bold justify-self-end text-secondary-dark-blue hover:text-primary-light-blue' />
                     </div>
@@ -36,8 +36,8 @@ const PatientCard = ({data}: Props) => {
                     <div onClick={() => handleClick('order', data.id.toString())} className='grid-cols-1 grid items-center gap-4 sm:grid-cols-2 sm:gap-2 md:grid-cols-[100px_150px_150px_150px_150px_1fr_24px]'>
                         <RiListUnordered  size={48} className='font-bold text-secondary-dark-blue md:block sm:hidden'/>
                         <h1 className='font-bold text-base text-secondary-dark-blue truncate'>{data.id}</h1>
-                        <h1 className='font-bold text-base text-secondary-dark-blue truncate'>{data.patient.lastName}</h1>
                         <h1 className='font-bold text-base text-secondary-dark-blue truncate'>{data.patient.firstName}</h1>
+                        <h1 className='font-bold text-base text-secondary-dark-blue truncate'>{data.patient.lastName}</h1>
                         <h1 className='font-bold text-base text-secondary-dark-blue truncate'>{format(new Date(data.orderDate),'yyyy-MM-dd HH:mm')}</h1>
                         <h1 className='font-bold text-base text-secondary-dark-blue truncate'>{data.orderState}</h1>
                         <IoIosArrowForward size={24} className='font-bold justify-self-end text-secondary-dark-blue hover:text-primary-light-blue' />
