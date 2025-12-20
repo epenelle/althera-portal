75d332fd2258368e8d1d605e12e3951abe54b472
epenelle <etienne.penelle@gmail.com>
2025-12-20 15:31:16 -0500

    add test controller

---
+++ app/backend/azure-functions/TestController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.Functions.Worker;
+
+namespace azure_functions;
+
+public class TestController
+{
+    [Function("hello")]
+    public async Task<IActionResult> Run([HttpTrigger(AuthorizationLevel.Function, "post", Route = "hello")] HttpRequest req, CancellationToken ct)
+    {
+        return new CreatedResult("/hello", new { message = "Hello, World!" });
+    }
+}
