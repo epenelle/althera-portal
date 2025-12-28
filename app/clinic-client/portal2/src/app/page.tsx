f889dd4ac22ea305ff3fa10f692a18430df8b959
epenelle <etienne.penelle@gmail.com>
2025-12-28 13:28:20 -0500

    Fix missong patients property on CreateOrderFormProps

--- app/clinic-client/portal2/src/app/page.tsx
+++ app/clinic-client/portal2/src/app/page.tsx
@@ -1,7 +1,10 @@
 import CreateOrderForm from '@/components/forms/CreateOrder';
 import { SheetTrigger } from '@/components/ui/sheet';
+import { patientClient } from '@/lib/api/patientClient';
 
-export default function Home() {
+export default async function Home() {
+    const patients = await patientClient.getPatients();
+
     return (
         <div>
             <header>
@@ -12,6 +15,8 @@ export default function Home() {
             <main>
                 <div className="mx-auto mt-4 grid h-40 w-4/5 gap-6 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3">
                     <CreateOrderForm
+                        // TODO: Not convinced by asking for patients here. Perhaps the component should fetch them itself ?
+                        patients={patients}
                         sheetTrigger={
                             <SheetTrigger className="rounded-xl bg-primary-dark-blue font-bold text-light-white hover:bg-secondary-dark-blue">
                                 Créer une commande
